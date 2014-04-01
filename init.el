a52369c5cb169385989b6f5e18d006890a9484c0
Amuro Nishizawa <aburos.d@gmail.com>
2014-04-01 15:00:49 +0900

    Customized for windows.

--- init.el
+++ init.el
@@ -9,6 +9,44 @@
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path)))
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'cl)
+(defvar installing-package-list
+  '(
+    init-loader
+    revive
+    haskell-mode
+    google-c-style
+    js2-mode
+    markdown-mode
+    multi-web-mode
+    magit
+    multi-term
+    nodejs-repl
+    tabbar
+    direx
+    anything
+    color-theme
+    color-theme-solarized
+    color-theme-sanityinc-tomorrow
+    csharp-mode
+    d-mode
+    flymake
+    helm
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                           when (not (package-installed-p x))
+                           collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+      (package-install pkg))))
+
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
--- inits/41-appearance.el
+++ inits/41-appearance.el
@@ -5,7 +5,7 @@
 (auto-image-file-mode t)
 
 ;; hide menubar/toolbar/scrollbar
-(menu-bar-mode t)
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 
 ;(set-scroll-bar-mode nil)
@@ -27,34 +27,15 @@
 ;; disable startup buffer
 (setq inhibit-startup-message t)
 (setq-default indent-tabs-mode nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#1d1f21"))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
- '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "66bd7fc2ed32703a332d05f5d2af5c30c12ff4e729d77d8271b91d6f6f7e15fc" "7fa9dc3948765d7cf3d7a289e40039c2c64abf0fad5c616453b263b601532493" default)))
- '(fci-rule-color "#282a2e")
- '(fringe-mode 6 nil (fringe))
- '(linum-format " %7d ")
- '(main-line-color1 "#191919")
- '(main-line-color2 "#111111")
- '(powerline-color1 "#191919")
- '(powerline-color2 "#111111")
- '(tab-width 2)
- '(vc-annotate-background nil)
- '(vc-annotate-color-map (quote ((20 . "#cc6666") (40 . "#de935f") (60 . "#f0c674") (80 . "#b5bd68") (100 . "#8abeb7") (120 . "#81a2be") (140 . "#b294bb") (160 . "#cc6666") (180 . "#de935f") (200 . "#f0c674") (220 . "#b5bd68") (240 . "#8abeb7") (260 . "#81a2be") (280 . "#b294bb") (300 . "#cc6666") (320 . "#de935f") (340 . "#f0c674") (360 . "#b5bd68")))))
 
 ;; font settings
 ;; ABCDEFGHIJKLMNOP
 ;; abcdefghijklmnop
 ;; 0123456789012345
-;; ï½¤ï½¢ï½¤ï½¤ï½¤ï½¦ï½¤ï½¨ï½¤ï½ªï½¤ï½«ï½¤ï½­ï½¤ï½¯
+;; ‚ ‚¢‚¤‚¦‚¨
 (set-face-attribute 'default nil
                     :family "Migu 1M Regular"
-                    :height 100)
+                    :height 110)
 (set-fontset-font
  nil 'japanese-jisx0208
  (font-spec :family "Migu 1M Regular"))
