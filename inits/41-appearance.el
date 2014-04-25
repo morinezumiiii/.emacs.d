d50d0c433d646a802992902bc6dd8946b78a1908
Amuro Nishizawa <aburos.d@gmail.com>
2014-04-25 09:27:08 +0900

    display for japanese font improved.

--- inits/41-appearance.el
+++ inits/41-appearance.el
@@ -33,11 +33,23 @@
 ;; abcdefghijklmnop
 ;; 0123456789012345
 ;; ‚ ‚¢‚¤‚¦‚¨
-(set-face-attribute 'default nil
-                    :family "Migu 1M Regular"
-                    :height 110)
-(set-fontset-font
- nil 'japanese-jisx0208
- (font-spec :family "Migu 1M Regular"))
-(setq face-font-rescale-alist
-      '((".*Migu 1M Regular*." . 1.0)))
+;(set-face-attribute 'default nil
+;                    :family "Migu 1M Regular"
+;                    :height 110)
+;(set-fontset-font
+; nil 'japanese-jisx0208
+; (font-spec :family "Migu 1M Regular"))
+;(setq face-font-rescale-alist
+;      '((".*Migu 1M Regular*." . 1.0)))
+(set-face-attribute 'default nil :family "Inconsolata" :height 115)
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  (cons "Ricty Discord" "iso10646-1"))
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0212
+                  (cons "Ricty Discord" "iso10646-1"))
+(set-fontset-font (frame-parameter nil 'font)
+                  'katakana-jisx0201
+                  (cons "Ricty Discord" "iso10646-1"))
+(setq face-font-rescase-alist
+      ((".*Ricty*.". 1.0)))
